cfa0a87d3f8679a06a443c504469488eda959fee
Monique Parnis <monique.parnis@gmail.com>
2023-07-18 11:30:03 +0000

    Add node fs module to test more than 1 DOM component

--- scripts/tests/button.test.js
+++ scripts/tests/button.test.js
@@ -6,7 +6,13 @@ const buttonClick = require("../button");
 
 // runs before each test is run - creates a mock DOM
 beforeEach( () => {
-    document.body.innerHTML = "<p id='par'></p>";
+    // add in the node fs module which allows us to open, read and write files
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+    // document.body.innerHTML = "<p id='par'></p>";
 });
 
 describe("DOM tests", () => {
@@ -14,4 +20,7 @@ describe("DOM tests", () => {
         buttonClick();
         expect(document.getElementById("par").innerHTML).toEqual("You Clicked");
     });
-});
+    test("h1 should exist", () => {
+        expect(document.getElementsByTagName("h1").length).toBe(1);
+    });
+}); 
